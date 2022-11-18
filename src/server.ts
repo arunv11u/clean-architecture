280a19509a46300696fd63ab0efae2542f524340
Arun Varadharajalu <arun@ruahtech.com.au>
2022-11-18 13:12:09 +0530

    Remove https server config

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
 import express from 'express';
 import http from 'node:http';
 import https from 'node:https';
-import fs from 'fs';
-import { Environment } from './utils';
 
 const app = express();
 let server: http.Server | https.Server;
 
-const environment = process.env.NODE_ENV;
-
-if (environment === Environment.PRODUCTION || environment === Environment.STAGING) {
-    const options = {
-        key: fs.readFileSync('/opt/key.pem', 'utf8'),
-        cert: fs.readFileSync('/opt/cert.pem', 'utf8')
-    };
-
-    server = https.createServer(options, app);
-} else server = http.createServer(app);
+server = http.createServer(app);
 
 export { server, app };
