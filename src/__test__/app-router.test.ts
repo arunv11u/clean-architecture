ad07e1dca90530d647091b18050d593358ac9b12
Arun Varadharajalu <arun@ruahtech.com.au>
2022-11-19 21:33:34 +0530

    Add test suites to App Router, App and Server modules

---
+++ src/__test__/app-router.test.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { AppRouter } from "../app-router";
+
+
+describe("App Router Class", () => {
+    describe(`"getInstance" method`, () => {
+
+        describe("Happy Path", () => {
+            it("No input has passed, should return router object", () => {
+                const appRouter = AppRouter.getInstance();
+
+                expect(AppRouter.router).toBe(express.Router);
+                expect(AppRouter.getInstance()).toBe(appRouter);
+            });
+        });
+
+    });
+});
---
+++ src/__test__/server.test.ts
@@ -0,0 +1,20 @@
+import http from 'node:http';
+import express from 'express';
+import { server, app } from '../server';
+
+describe("Server Module", () => {
+    describe("Create Server fn", () => {
+        describe("Happy Path", () => {
+            it("Needed Http Server, server should be Http Server", () => {
+                expect(server).toStrictEqual(http.createServer(app));
+            });
+
+            it("Needed Express Application, app should be express app", () => {
+                const _app = express();
+                expect(app.name).toBe(_app.name);
+            });
+        });
+    });
+});
+
+
