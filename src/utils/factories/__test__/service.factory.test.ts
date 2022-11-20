a280c6b403c9af2d8cf0fecb64dfe9637dbb9bae
Arun Varadharajalu <arun@ruahtech.com.au>
2022-11-20 11:28:03 +0530

    Add service factory test suite

--- src/utils/factories/__test__/service.factory.test.ts
+++ src/utils/factories/__test__/service.factory.test.ts
@@ -1,19 +1,17 @@
-// import { UserService } from '../../../users';
-// import { ServiceFactory } from '../service.factory';
+import { serviceFactory } from '../../../global-config';
+import { UserService } from '../../../users';
 
-// const serviceFactory = ServiceFactory.getInstance();
 
-// describe("Factory Module", () => {
-//     describe("Service Factory", () => {
-//         describe(`"getUserService" method`, () => {
-//             describe("Happy Path", () => {
-//                 it("No input has passed, should return user pipe object", () => { 
-//                     const userService = serviceFactory.getUserService();
-//                     expect(userService).toBeInstanceOf(UserService);
-//                 });
-//             });
-//         });
-//     });
-// });
+describe("Factory Module", () => {
+    describe("Service Factory", () => {
+        describe(`"getUserService" method`, () => {
+            describe("Happy Path", () => {
+                it("No input has passed, should return user service object", () => { 
+                    const userService = serviceFactory.getUserService();
+                    expect(userService).toBeInstanceOf(UserService);
+                });
+            });
+        });
+    });
+});
 
-it("", () => { });
