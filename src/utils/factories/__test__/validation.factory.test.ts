7e70b672167acdd744e9d9dc6bf09bf87e423bb4
Arun Varadharajalu <arun@ruahtech.com.au>
2022-11-20 11:53:20 +0530

    Add test suite validation factory

---
+++ src/utils/factories/__test__/validation.factory.test.ts
@@ -0,0 +1,17 @@
+import { validationFactory } from '../../../global-config';
+import { UserValidation } from '../../../users';
+
+
+describe("Factory Module", () => {
+    describe("Validation Factory", () => {
+        describe(`"getUserValidation" method`, () => {
+            describe("Happy Path", () => {
+                it("No input has passed, should return user validation object", () => { 
+                    const userValidation = validationFactory.getUserValidation();
+                    expect(userValidation).toBeInstanceOf(UserValidation);
+                });
+            });
+        });
+    });
+});
+
