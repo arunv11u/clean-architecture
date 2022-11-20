5f1b223591471ea376a81699c536ec7e749c59e1
Arun Varadharajalu <arun@ruahtech.com.au>
2022-11-21 01:03:11 +0530

    Add sample use decorator in user controller

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,21 +1,42 @@
 import { Response, NextFunction } from "express";
-import { BaseUserController, BaseServiceFactory, BaseResponse, Controller, ServiceFactory, Use, Post, ValidationFactory, BaseValidationFactory, BasePipeFactory, PipeFactory, TypedRequest, UserDTO } from "../utils";
+import {
+  BaseUserController,
+  BaseServiceFactory,
+  BaseResponse,
+  Controller,
+  ServiceFactory,
+  Use,
+  Post,
+  ValidationFactory,
+  BaseValidationFactory,
+  BasePipeFactory,
+  PipeFactory,
+  TypedRequest,
+  UserDTO,
+} from "../utils";
 
-const validationFactory: BaseValidationFactory = ValidationFactory.getInstance();
+const validationFactory: BaseValidationFactory =
+  ValidationFactory.getInstance();
 const pipeFactory: BasePipeFactory = PipeFactory.getInstance();
 const serviceFactory: BaseServiceFactory = ServiceFactory.getInstance();
 
-
 @Controller("/user")
 class UserController implements BaseUserController {
+  @Post("/")
+  @Use(
+    (
+      request: TypedRequest<{}, {}, {}>,
+      response: Response,
+      next: NextFunction
+    ) => {
+      next();
+    }
+  )
+  register(
+    request: TypedRequest<{}, {}, UserDTO.Register>,
+    response: Response<any, Record<string, any>>,
+    next: NextFunction
+  ): void {}
+}
 
-    @Post("/")
-    register(request: TypedRequest<{}, {}, UserDTO.Register>, response: Response<any, Record<string, any>>, next: NextFunction): void {
-        
-    };
-
-};
-
-export {
-    UserController
-};
+export { UserController };
