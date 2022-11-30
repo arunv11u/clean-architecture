cd0ab389438c2e4adfef1a19023ab01752b67e46
Arun Varadharajalu <arun@ruahtech.com.au>
2022-11-30 12:57:30 +0530

    Update port config in both production and staging environment

--- src/utils/configs/prod.config.ts
+++ src/utils/configs/prod.config.ts
@@ -1,5 +1,5 @@
 export const prodConfig = {
-    port: 80,
+    port: 3000,
     rateLimiterWindowMs: 5 * 60 * 1000,
     rateLimiterMaxRequests: 50
 };
--- src/utils/configs/staging.config.ts
+++ src/utils/configs/staging.config.ts
@@ -1,5 +1,5 @@
 export const stagingConfig = {
-    port: 443,
+    port: 3000,
     rateLimiterWindowMs: 5 * 60 * 1000,
     rateLimiterMaxRequests: 50
 };
