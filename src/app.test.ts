4fd051e4f700f4027c555e169349488e4f449e5e
Arun Varadharajalu <arun@ruahtech.com.au>
2022-12-02 09:09:21 +0530

    Update test suite for app module

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,3 +1,4 @@
+import { mockProcessExit } from "jest-mock-process";
 import mockLoaderImpl, { mockLoad } from './utils/__mocks__/loader';
 import { App } from './app';
 
@@ -12,18 +13,48 @@ jest.mock('./utils/loader', () => {
 jest.mock('./server', () => {
     return {
         server: {
-            listen: jest.fn()
+            listen: jest.fn((port, cb) => cb())
         }
     };
 });
+
+let mockExit = mockProcessExit();
 
 describe("App Module", () => {
     let app = new App();
 
     describe(`"main" method`, () => {
         describe("Happy Path", () => {
-            it("Should start up the server", async () => {
+            it("If env is other than dev or staging, Should start up the server in the production environment", async () => {
                 await app.main();
+
+                expect(mockLoad).toHaveBeenCalled();
+            });
+
+            it("If env is dev, Should start up the server in the dev environment", async () => {
+                process.env.NODE_ENV = "dev";
+
+                await app.main();
+
+                expect(mockLoad).toHaveBeenCalled();
+
+                process.env.NODE_ENV = "test";
+            });
+
+            it("If env is staging, Should start up the server in the dev environment", async () => {
+                process.env.NODE_ENV = "staging";
+
+                await app.main();
+
+                expect(mockLoad).toHaveBeenCalled();
+
+                process.env.NODE_ENV = "test";
+            });
+
+            it("Error while loading modules, should exit the process", async () => {
+                mockLoad.mockImplementation(() => { throw new Error() });
+
+                expect(app.main()).rejects.toBeInstanceOf(Error);
 
                 expect(mockLoad).toHaveBeenCalled();
             });
