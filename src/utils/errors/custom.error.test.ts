4929710e199c506774f15eebcc60819bd272cdaa
Arun Varadharajalu <arun@ruahtech.com.au>
2022-12-08 14:57:01 +0530

    Update custom error test suite

--- src/utils/errors/custom.error.test.ts
+++ src/utils/errors/custom.error.test.ts
@@ -1,11 +1,29 @@
+import { ErrorObject } from "../types";
 import { CustomError } from "./custom.error";
 
+class CustomErrorImpl extends CustomError {
+    reason = 'Custom Error';
+    statusCode = 500;
+    constructor() {
+        super('Custom Error');
+
+        // Only because extending from a built in class
+        Object.setPrototypeOf(this, CustomErrorImpl.prototype);
+    };
+
+    serializeErrors(): ErrorObject[] {
+        return [{ message: this.reason }];
+    };
+};
 
 describe("Error Module", () => {
     describe(`"CustomError" class`, () => {
         describe("Happy Path", () => {
             it("Need a class which extended built-in Error class, CustomError class should extend Error class", () => {
+                const customError = new CustomErrorImpl();
+                
                 expect(CustomError.prototype).toBeInstanceOf(Error);
+                expect(customError.message).toBe("Custom Error");
             });
         });
     });
