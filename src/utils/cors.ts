a0019dae904a10910c8d1181126856c8d2873801
Arun Varadharajalu <arun@ruahtech.com.au>
2022-12-15 13:02:21 +0530

    Update cors with credentails true config

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -19,7 +19,8 @@ export const corsOptions = function (req: any, callback: any) {
             if (whitelist.indexOf(origin) !== -1 || !origin) callback(null, true);
             else callback(new Error('Not allowed by CORS'));
         },
-        allowedHeaders: ["Content-Type", "Authorization"]
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true
     };
     callback(null, corsOptions);
 
